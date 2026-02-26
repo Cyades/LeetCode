e7c8daf7e91f10f96ecb4a2db81adaa9922f5770
patriciatyas <patriciatyas11@gmail.com>
2026-02-27 00:24:27 +0700

    tyuz: 9

---
+++ Teeyush/Easy/9.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/palindrome-number/submissions/1932052730/
+class Solution(object):
+    def isPalindrome(self, x):
+        x = str(x)
+        i, j = 0, len(x)-1
+        
+        while i < j:
+            if not x[i] == x[j]:
+                return False
+            i += 1
+            j -= 1
+        
+        return True
